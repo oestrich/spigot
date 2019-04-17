c296723ef455777c1783f12521b12e76e3afaa5e
Eric Oestrich <eric@oestrich.org>
2019-04-16 20:39:03 -0400

    Set up distillery

--- mix.exs
+++ mix.exs
@@ -23,6 +23,7 @@ defmodule Spigot.MixProject do
   defp deps do
     [
       {:credo, "~> 1.0", only: [:dev, :test], runtime: false},
+      {:distillery, "~> 2.0"},
       {:elixir_uuid, "~> 1.2"},
       {:jason, "~> 1.1"},
       {:ranch, "~> 1.7"},

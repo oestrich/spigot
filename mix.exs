4dd3a6001c65f6e762fae2db3805f007deb5ef29
Eric Oestrich <eric@oestrich.org>
2019-04-21 00:26:39 -0400

    Require elixir 1.8 for fetching docs

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Spigot.MixProject do
     [
       app: :spigot,
       version: "0.1.0",
-      elixir: "~> 1.6",
+      elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]

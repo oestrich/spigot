9501505c1e482c097d42def97e93e13741732d7a
Eric Oestrich <eric@oestrich.org>
2019-04-29 18:32:57 -0400

    Switch to using no assign macro

--- lib/spigot/views/who.ex
+++ lib/spigot/views/who.ex
@@ -1,10 +1,10 @@
 defmodule Spigot.Views.Who do
   use Spigot, :view
 
-  def render("who", assigns) do
+  def render("who", %{players: players}) do
     ~E"""
     Other players online:
-    <%= Enum.map(@players, fn player -> %>
+    <%= Enum.map(players, fn player -> %>
       - <%= IO.ANSI.white() %><%= player.name %><%= IO.ANSI.reset() %>
     <% end) %>
     """

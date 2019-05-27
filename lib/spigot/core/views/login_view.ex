22e5b9b78da2deea009e60b9b2b88fd08e429576
Eric Oestrich <eric@oestrich.org>
2019-05-27 11:47:51 -0400

    Spigot name is true color grapevine purple

--- lib/spigot/core/views/login_view.ex
+++ lib/spigot/core/views/login_view.ex
@@ -2,7 +2,7 @@ defmodule Spigot.Core.LoginView do
   use Spigot, :view
 
   def render("logged-in", %{username: username}) do
-    "\nWelcome to #{IO.ANSI.cyan()}Spigot#{IO.ANSI.reset()}, #{username}.\n"
+    "\nWelcome to \e[38;2;169;114;218mSpigot#{IO.ANSI.reset()}, #{username}.\n"
   end
 
   def render("welcome", _assigns) do

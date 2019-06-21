9c311124b9b4c198bfa651547ac40cea59321731
Eric Oestrich <eric@oestrich.org>
2019-06-21 14:21:42 -0400

    Tag docker with the git sha

--- docker.sh
+++ docker.sh
@@ -1,9 +1,12 @@
 #/bin/bash
 set -e
 
+sha=`git rev-parse master`
+
 rm -r tmp/build
 mkdir -p tmp/build
 git archive master | tar x -C tmp/build/
 cd tmp/build
 
-docker build --build-arg cookie=cookie -t oestrich/spigot:master .
+docker build --build-arg cookie=cookie -t oestrich/spigot:${sha} .
+docker push oestrich/spigot:${sha}

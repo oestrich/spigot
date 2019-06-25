8391b2a504580c3262b56ca90ce0eb325baa56fb
Eric Oestrich <eric@oestrich.org>
2019-06-25 14:26:03 -0400

    More text on the base look

--- lib/spigot/core/views/look_view.ex
+++ lib/spigot/core/views/look_view.ex
@@ -2,6 +2,6 @@ defmodule Spigot.Core.LookView do
   use Spigot, :view
 
   def render("base", _assigns) do
-    "You are in a void.\n"
+    "You are in a void, there is nothing around you.\n"
   end
 end
